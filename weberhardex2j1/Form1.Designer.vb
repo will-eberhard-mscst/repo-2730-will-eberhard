8bef5e3d0c6aded57d23d8957f1d8d6972ab90d2
wille <willeberhard@hotmail.com>
2016-11-11 19:04:03 -0600

    done

---
+++ weberhardex2j1/Form1.Designer.vb
@@ -0,0 +1,387 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.btnDisplayDelivery = New System.Windows.Forms.Button()
+        Me.txtPartNum6 = New System.Windows.Forms.TextBox()
+        Me.txtPartNum5 = New System.Windows.Forms.TextBox()
+        Me.txtPartNum4 = New System.Windows.Forms.TextBox()
+        Me.txtPartNum3 = New System.Windows.Forms.TextBox()
+        Me.txtPartNum2 = New System.Windows.Forms.TextBox()
+        Me.lstDelivery = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtPartNum1 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnProper = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lstColors = New System.Windows.Forms.ListBox()
+        Me.txtItem5 = New System.Windows.Forms.TextBox()
+        Me.txtItem4 = New System.Windows.Forms.TextBox()
+        Me.txtItem3 = New System.Windows.Forms.TextBox()
+        Me.txtItem2 = New System.Windows.Forms.TextBox()
+        Me.btnDisplayColor = New System.Windows.Forms.Button()
+        Me.txtItem1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(165, 397)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(107, 30)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.btnDisplayDelivery)
+        Me.GroupBox3.Controls.Add(Me.txtPartNum6)
+        Me.GroupBox3.Controls.Add(Me.txtPartNum5)
+        Me.GroupBox3.Controls.Add(Me.txtPartNum4)
+        Me.GroupBox3.Controls.Add(Me.txtPartNum3)
+        Me.GroupBox3.Controls.Add(Me.txtPartNum2)
+        Me.GroupBox3.Controls.Add(Me.lstDelivery)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.txtPartNum1)
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Location = New System.Drawing.Point(262, 150)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(223, 242)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Proper Case: p495 #6"
+        '
+        'btnDisplayDelivery
+        '
+        Me.btnDisplayDelivery.Location = New System.Drawing.Point(61, 201)
+        Me.btnDisplayDelivery.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnDisplayDelivery.Name = "btnDisplayDelivery"
+        Me.btnDisplayDelivery.Size = New System.Drawing.Size(107, 30)
+        Me.btnDisplayDelivery.TabIndex = 7
+        Me.btnDisplayDelivery.Text = "Display &Delivery"
+        '
+        'txtPartNum6
+        '
+        Me.txtPartNum6.Location = New System.Drawing.Point(13, 172)
+        Me.txtPartNum6.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtPartNum6.Name = "txtPartNum6"
+        Me.txtPartNum6.Size = New System.Drawing.Size(77, 20)
+        Me.txtPartNum6.TabIndex = 6
+        Me.txtPartNum6.Text = "66AA"
+        '
+        'txtPartNum5
+        '
+        Me.txtPartNum5.Location = New System.Drawing.Point(13, 148)
+        Me.txtPartNum5.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtPartNum5.Name = "txtPartNum5"
+        Me.txtPartNum5.Size = New System.Drawing.Size(77, 20)
+        Me.txtPartNum5.TabIndex = 5
+        Me.txtPartNum5.Text = "55u"
+        '
+        'txtPartNum4
+        '
+        Me.txtPartNum4.Location = New System.Drawing.Point(13, 123)
+        Me.txtPartNum4.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtPartNum4.Name = "txtPartNum4"
+        Me.txtPartNum4.Size = New System.Drawing.Size(77, 20)
+        Me.txtPartNum4.TabIndex = 4
+        Me.txtPartNum4.Text = "44Fo"
+        '
+        'txtPartNum3
+        '
+        Me.txtPartNum3.Location = New System.Drawing.Point(13, 98)
+        Me.txtPartNum3.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtPartNum3.Name = "txtPartNum3"
+        Me.txtPartNum3.Size = New System.Drawing.Size(77, 20)
+        Me.txtPartNum3.TabIndex = 3
+        Me.txtPartNum3.Text = "33fS"
+        '
+        'txtPartNum2
+        '
+        Me.txtPartNum2.Location = New System.Drawing.Point(13, 73)
+        Me.txtPartNum2.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtPartNum2.Name = "txtPartNum2"
+        Me.txtPartNum2.Size = New System.Drawing.Size(77, 20)
+        Me.txtPartNum2.TabIndex = 2
+        Me.txtPartNum2.Text = "22mp"
+        '
+        'lstDelivery
+        '
+        Me.lstDelivery.FormattingEnabled = True
+        Me.lstDelivery.Location = New System.Drawing.Point(98, 47)
+        Me.lstDelivery.Margin = New System.Windows.Forms.Padding(2)
+        Me.lstDelivery.Name = "lstDelivery"
+        Me.lstDelivery.Size = New System.Drawing.Size(111, 147)
+        Me.lstDelivery.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(95, 27)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(86, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Delivery method:"
+        '
+        'txtPartNum1
+        '
+        Me.txtPartNum1.Location = New System.Drawing.Point(13, 47)
+        Me.txtPartNum1.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtPartNum1.Name = "txtPartNum1"
+        Me.txtPartNum1.Size = New System.Drawing.Size(77, 20)
+        Me.txtPartNum1.TabIndex = 1
+        Me.txtPartNum1.Text = "11MS"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(10, 27)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(67, 13)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "&Part number:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnProper)
+        Me.GroupBox2.Controls.Add(Me.lblName)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.txtName)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(324, 125)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Proper Case: p495 #5"
+        '
+        'btnProper
+        '
+        Me.btnProper.Location = New System.Drawing.Point(221, 38)
+        Me.btnProper.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnProper.Name = "btnProper"
+        Me.btnProper.Size = New System.Drawing.Size(87, 30)
+        Me.btnProper.TabIndex = 2
+        Me.btnProper.Text = "&Proper Case"
+        '
+        'lblName
+        '
+        Me.lblName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblName.Location = New System.Drawing.Point(18, 86)
+        Me.lblName.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(189, 25)
+        Me.lblName.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 67)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Name (Proper Case):"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(19, 44)
+        Me.txtName.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(188, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 23)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(89, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "&Name (First Last):"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lstColors)
+        Me.GroupBox1.Controls.Add(Me.txtItem5)
+        Me.GroupBox1.Controls.Add(Me.txtItem4)
+        Me.GroupBox1.Controls.Add(Me.txtItem3)
+        Me.GroupBox1.Controls.Add(Me.txtItem2)
+        Me.GroupBox1.Controls.Add(Me.btnDisplayColor)
+        Me.GroupBox1.Controls.Add(Me.txtItem1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 150)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(244, 222)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Colors: p495 #2"
+        '
+        'lstColors
+        '
+        Me.lstColors.FormattingEnabled = True
+        Me.lstColors.Location = New System.Drawing.Point(101, 47)
+        Me.lstColors.Name = "lstColors"
+        Me.lstColors.Size = New System.Drawing.Size(128, 121)
+        Me.lstColors.TabIndex = 8
+        '
+        'txtItem5
+        '
+        Me.txtItem5.Location = New System.Drawing.Point(16, 148)
+        Me.txtItem5.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtItem5.Name = "txtItem5"
+        Me.txtItem5.Size = New System.Drawing.Size(77, 20)
+        Me.txtItem5.TabIndex = 5
+        Me.txtItem5.Text = "3333333"
+        '
+        'txtItem4
+        '
+        Me.txtItem4.Location = New System.Drawing.Point(16, 123)
+        Me.txtItem4.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtItem4.Name = "txtItem4"
+        Me.txtItem4.Size = New System.Drawing.Size(77, 20)
+        Me.txtItem4.TabIndex = 4
+        Me.txtItem4.Text = "bbbWbbb"
+        '
+        'txtItem3
+        '
+        Me.txtItem3.Location = New System.Drawing.Point(16, 98)
+        Me.txtItem3.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtItem3.Name = "txtItem3"
+        Me.txtItem3.Size = New System.Drawing.Size(77, 20)
+        Me.txtItem3.TabIndex = 3
+        Me.txtItem3.Text = "aaaraaa"
+        '
+        'txtItem2
+        '
+        Me.txtItem2.Location = New System.Drawing.Point(16, 73)
+        Me.txtItem2.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtItem2.Name = "txtItem2"
+        Me.txtItem2.Size = New System.Drawing.Size(77, 20)
+        Me.txtItem2.TabIndex = 2
+        Me.txtItem2.Text = "222G222"
+        '
+        'btnDisplayColor
+        '
+        Me.btnDisplayColor.Location = New System.Drawing.Point(46, 177)
+        Me.btnDisplayColor.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnDisplayColor.Name = "btnDisplayColor"
+        Me.btnDisplayColor.Size = New System.Drawing.Size(107, 30)
+        Me.btnDisplayColor.TabIndex = 6
+        Me.btnDisplayColor.Text = "Display &Color"
+        '
+        'txtItem1
+        '
+        Me.txtItem1.Location = New System.Drawing.Point(16, 48)
+        Me.txtItem1.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtItem1.Name = "txtItem1"
+        Me.txtItem1.Size = New System.Drawing.Size(77, 20)
+        Me.txtItem1.TabIndex = 1
+        Me.txtItem1.Text = "111b111"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(97, 27)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Item color:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 28)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Item number:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(498, 442)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents btnDisplayDelivery As Button
+    Friend WithEvents txtPartNum6 As TextBox
+    Friend WithEvents txtPartNum5 As TextBox
+    Friend WithEvents txtPartNum4 As TextBox
+    Friend WithEvents txtPartNum3 As TextBox
+    Friend WithEvents txtPartNum2 As TextBox
+    Friend WithEvents lstDelivery As ListBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtPartNum1 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnProper As Button
+    Friend WithEvents lblName As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lstColors As ListBox
+    Friend WithEvents txtItem5 As TextBox
+    Friend WithEvents txtItem4 As TextBox
+    Friend WithEvents txtItem3 As TextBox
+    Friend WithEvents txtItem2 As TextBox
+    Friend WithEvents btnDisplayColor As Button
+    Friend WithEvents txtItem1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ weberhardex2j1/Form1.vb
@@ -0,0 +1,121 @@
+﻿' Name:         Chapter 8, String functions, list boxes
+' Purpose:      #2: Interprets color code
+'               #5: Sets proper upper/lower case
+'               #6: Interprets shipping code
+' Programmer:   <your name> on <today's date>
+
+Option Strict On
+Option Explicit On
+Option Infer Off
+
+Public Class Form1
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ItemNumbers_TextChanged(sender As Object, e As EventArgs) _
+            Handles txtItem1.TextChanged, txtItem2.TextChanged, txtItem3.TextChanged, txtItem4.TextChanged, txtItem5.TextChanged
+        lstColors.Items.Clear()
+    End Sub
+
+    Private Sub btnDisplayColor_Click(sender As Object, e As EventArgs) Handles btnDisplayColor.Click
+        DisplayColor(txtItem1.Text)
+        DisplayColor(txtItem2.Text)
+        DisplayColor(txtItem3.Text)
+        DisplayColor(txtItem4.Text)
+        DisplayColor(txtItem5.Text)
+    End Sub
+
+    Private Sub DisplayColor(ByVal itemNum As String)
+        If itemNum.Length <> 7 Then
+            MessageBox.Show("The value must contain 7 characters", "Try Again")
+        ElseIf itemNum.ToUpper Like "???B???" Then
+            lstColors.Items.Add(itemNum & ": Blue")
+        ElseIf itemNum.ToUpper Like "???G???" Then
+            lstColors.Items.Add(itemNum & ": Green")
+        ElseIf itemNum.ToUpper Like "???R???" Then
+            lstColors.Items.Add(itemNum & ": Red")
+        ElseIf itemNum.ToUpper Like "???W???" Then
+            lstColors.Items.Add(itemNum & ": White")
+        Else
+            lstColors.Items.Add("Invalid: " & itemNum)
+        End If
+
+
+    End Sub
+
+    Private Sub txtName_Enter(sender As Object, e As EventArgs) Handles txtName.Enter
+        txtName.SelectAll()
+    End Sub
+
+    Private Sub txtName_TextChanged(sender As Object, e As EventArgs) Handles txtName.TextChanged
+        lblName.Text = String.Empty
+    End Sub
+
+    Private Sub btnDisplayDelivery_Click(sender As Object, e As EventArgs) Handles btnDisplayDelivery.Click
+        DisplayDelivery(txtPartNum1.Text)
+        DisplayDelivery(txtPartNum2.Text)
+        DisplayDelivery(txtPartNum3.Text)
+        DisplayDelivery(txtPartNum4.Text)
+        DisplayDelivery(txtPartNum5.Text)
+        DisplayDelivery(txtPartNum6.Text)
+    End Sub
+
+    Private Sub DisplayDelivery(ByVal partNum As String)
+        If partNum.Length = 3 OrElse partNum.Length = 4 Then
+            If partNum.ToUpper Like "##MS" Then
+                lstDelivery.Items.Add("Mail - Standard")
+            End If
+            If partNum.ToUpper Like "##MP" Then
+                lstDelivery.Items.Add("Mail - Priority")
+            End If
+            If partNum.ToUpper Like "##FS" Then
+                lstDelivery.Items.Add("FedEx - Standarf")
+            End If
+            If partNum.ToUpper Like "##FO" Then
+                lstDelivery.Items.Add("FedEx - Overnight")
+            End If
+            If partNum.ToUpper Like "##U" Then
+                lstDelivery.Items.Add("UPS")
+            End If
+        Else
+            lstDelivery.Items.Add("Invalid")
+        End If
+
+    End Sub
+
+    Private Sub PartNum_TextChanged(sender As Object, e As EventArgs) _
+            Handles txtPartNum1.TextChanged, txtPartNum2.TextChanged, txtPartNum3.TextChanged, txtPartNum4.TextChanged, txtPartNum5.TextChanged, txtPartNum6.TextChanged
+        lstDelivery.Items.Clear()
+    End Sub
+
+    'btnProper_Click pseudocode:
+    '1) Convert txtName to lower case, store in strName
+    '2) Use strName.Substring() to get first character of strName, convert to upper case, store in strFirstChar
+    '3) Remove first character of strName
+    '4) Insert strFirstChar at beginning of strName
+    '5) Use strName.IndexOf() to find the space character. Add 1 and store in index
+    '6) Repeat steps 2 - 4 to remove/insert first letter of last name
+
+    Private Sub btnProper_Click(sender As Object, e As EventArgs) Handles btnProper.Click
+        Dim strName As String = txtName.Text.ToLower
+        Dim index As Integer
+        Dim strFirstChar As String
+        Dim strLastChar As String
+        strFirstChar = strName.ToUpper.Substring(0, 1)
+        strName = strName.Remove(0, 1)
+        strName = strName.Insert(0, strFirstChar)
+
+        index = strName.IndexOf(" ")
+        If index <> -1 Then
+            index = index + 1
+            strLastChar = strName.ToUpper.Substring(index, 1)
+            strName = strName.Remove(index, 1)
+            strName = strName.Insert(index, strLastChar)
+        End If
+
+        lblName.Text = strName
+    End Sub
+
+End Class
