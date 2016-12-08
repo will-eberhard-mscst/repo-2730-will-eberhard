a1de71c3c05ee8fb2a5b0e7c0820750e9919f8d8
wille <willeberhard@hotmail.com>
2016-12-08 16:08:00 -0600

    Done

---
+++ weberhardex3e1/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblCommission = New System.Windows.Forms.Label()
+        Me.cboSearchValue = New System.Windows.Forms.ComboBox()
+        Me.cboSearchRanges = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblValue = New System.Windows.Forms.Label()
+        Me.lblRangeCount = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(139, 20)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "&Commission Values:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(32, 108)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 20)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "&Search for Value:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(26, 237)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(125, 20)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "&Search for Range:"
+        '
+        'lblCommission
+        '
+        Me.lblCommission.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCommission.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCommission.Location = New System.Drawing.Point(157, 9)
+        Me.lblCommission.Name = "lblCommission"
+        Me.lblCommission.Size = New System.Drawing.Size(240, 85)
+        Me.lblCommission.TabIndex = 17
+        '
+        'cboSearchValue
+        '
+        Me.cboSearchValue.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
+        Me.cboSearchValue.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboSearchValue.FormattingEnabled = True
+        Me.cboSearchValue.Items.AddRange(New Object() {"50", "100", "300", "900"})
+        Me.cboSearchValue.Location = New System.Drawing.Point(157, 108)
+        Me.cboSearchValue.Name = "cboSearchValue"
+        Me.cboSearchValue.Size = New System.Drawing.Size(117, 123)
+        Me.cboSearchValue.TabIndex = 18
+        '
+        'cboSearchRanges
+        '
+        Me.cboSearchRanges.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
+        Me.cboSearchRanges.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboSearchRanges.FormattingEnabled = True
+        Me.cboSearchRanges.Items.AddRange(New Object() {"0-200", "100-300", "700-800"})
+        Me.cboSearchRanges.Location = New System.Drawing.Point(157, 237)
+        Me.cboSearchRanges.Name = "cboSearchRanges"
+        Me.cboSearchRanges.Size = New System.Drawing.Size(117, 123)
+        Me.cboSearchRanges.TabIndex = 19
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(280, 108)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 20)
+        Me.Label4.TabIndex = 20
+        Me.Label4.Text = "&Count:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(280, 237)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 20)
+        Me.Label5.TabIndex = 21
+        Me.Label5.Text = "&Count:"
+        '
+        'lblValue
+        '
+        Me.lblValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblValue.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblValue.Location = New System.Drawing.Point(337, 107)
+        Me.lblValue.Name = "lblValue"
+        Me.lblValue.Size = New System.Drawing.Size(60, 29)
+        Me.lblValue.TabIndex = 22
+        Me.lblValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblRangeCount
+        '
+        Me.lblRangeCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRangeCount.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRangeCount.Location = New System.Drawing.Point(337, 233)
+        Me.lblRangeCount.Name = "lblRangeCount"
+        Me.lblRangeCount.Size = New System.Drawing.Size(60, 29)
+        Me.lblRangeCount.TabIndex = 23
+        Me.lblRangeCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(448, 388)
+        Me.Controls.Add(Me.lblRangeCount)
+        Me.Controls.Add(Me.lblValue)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cboSearchRanges)
+        Me.Controls.Add(Me.cboSearchValue)
+        Me.Controls.Add(Me.lblCommission)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblCommission As Label
+    Friend WithEvents cboSearchValue As ComboBox
+    Friend WithEvents cboSearchRanges As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblValue As Label
+    Friend WithEvents lblRangeCount As Label
+End Class
---
+++ weberhardex3e1/Form1.vb
@@ -0,0 +1,71 @@
+﻿Option Explicit On
+Option Strict On
+Option Infer Off
+'Will Eberhard 3e1 12/8/2016
+
+Public Class Form1
+    Private intCommission() As Integer = {300, 500, 200, 150, 600, 750, 900, 150, 100, 200, 250, 650, 300, 750, 800, 350, 250, 150, 100, 300}
+
+    Private Sub cboSearchRange_TextChanged(sender As Object, e As EventArgs) Handles cboSearchRanges.TextChanged
+        lblRangeCount.Text = String.Empty
+        Dim intSearchMin As Integer = 0
+        Dim intSearchMax As Integer = 0
+        Dim intHyphenIndex As Integer = cboSearchRanges.Text.IndexOf("-")
+        If intHyphenIndex >= 0 AndAlso cboSearchRanges.Text.Length > intHyphenIndex + 1 Then
+            Dim strSearchMin As String = cboSearchRanges.Text.Substring(0, intHyphenIndex).Trim()
+            Dim strSearchMax As String = cboSearchRanges.Text.Substring(intHyphenIndex + 1).Trim()
+            Int32.TryParse(strSearchMin, intSearchMin)
+            Int32.TryParse(strSearchMax, intSearchMax)
+            If intSearchMax >= intSearchMin Then
+                lblRangeCount.Text = CountRange(intSearchMin, intSearchMax).ToString
+            End If
+        End If
+
+    End Sub
+
+    Private Sub cboSearchValue_TextChanged(sender As Object, e As EventArgs) Handles cboSearchValue.TextChanged
+        Dim intValueCount As Integer
+
+        lblValue.Text = CountValue(intValueCount).ToString
+    End Sub
+
+    Private Sub DisplayCommissions()
+        For Each count As Integer In intCommission
+            lblCommission.Text &= count.ToString & ", "
+        Next count
+
+        'Removes extra comma at the end
+        If lblCommission.Text.EndsWith(", ") Then
+            lblCommission.Text = lblCommission.Text.Substring(0, lblCommission.Text.Length - 2)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        DisplayCommissions()
+        cboSearchRanges.SelectedIndex = 0
+        cboSearchValue.SelectedIndex = 0
+    End Sub
+
+    Private Function CountValue(intSearchValue As Integer) As Integer
+        Integer.TryParse(cboSearchValue.Text, intSearchValue)
+        Dim intValueCount As Integer = 0
+
+        For Each count As Integer In intCommission
+            If count = intSearchValue Then
+                intValueCount += 1
+            End If
+        Next count
+        Return intValueCount
+    End Function
+
+    Private Function CountRange(intSearchMin As Integer, intSearchMax As Integer) As Integer
+        Dim intRangeCount As Integer = 0
+
+        For Each count As Integer In intCommission
+            If count >= intSearchMin AndAlso count <= intSearchMax Then
+                intRangeCount += 1
+            End If
+        Next count
+        Return intRangeCount
+    End Function
+End Class
